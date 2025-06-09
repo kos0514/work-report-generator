2f12f038d9b971c1167ffe400c21533f84779c57
kos0514 <sato.kosuke514@gmail.com>
2025-06-09 20:15:37 +0900

    Initialize Spring Boot project with Gradle setup and essential configurations

---
+++ src/main/java/com/kos0514/work_report_generator/WorkReportGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.kos0514.work_report_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkReportGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkReportGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kos0514/work_report_generator/WorkReportGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kos0514.work_report_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkReportGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
